d0fe6740330d9f68fbd8ad4fea944d44871fc7de
wycliff byakagaba <spearkickboxing20@gmail.com>
2026-03-07 02:24:20 +0300

    week01/cse210-ww-student-template.sln  week01/Exercise1/cse210.csproj     week01/Exercise1/Program.cs     .vscode/launch.json

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // step 1: create avariable to hold the name of the user
+        string userName;
+        int userAge;
+        double height;
+        // step 2: ask the user for their name and store it in the variable
+        Console.Write("Please enter your name: ");
+        userName = Console.ReadLine();
+        // step 3: ask the user for their age and store it in the variable
+        Console.Write("Please enter your age: ");
+        userAge = int.Parse(Console.ReadLine());
+        // step 4: ask the user for their height and store it in the variable
+        Console.Write("Please enter your height (in meters): ");
+        height = double.Parse(Console.ReadLine());
+        // step 5: display the user's information
+        Console.WriteLine($"Hello, {userName}! You are {userAge} years old and {height} meters tall.");
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
     }
 }

4fadc53d6fb5ef69d3e29e0c253dfcb23e728310
wycliff byakagaba <spearkickboxing20@gmail.com>
2026-03-07 03:50:08 +0300

    week01/Exercise3/Program.cs week01/Exercise3/cse210.csproj

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
-class Program
+class Exercise3
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Prompt the user to enter a number
+        Console.Write("Enter a number to count up to: ");
+        int limit = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Counting from 1 to {limit}:");
+
+        for (int i = 1; i <= limit; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }

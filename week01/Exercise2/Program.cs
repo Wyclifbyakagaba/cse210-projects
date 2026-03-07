3e7f288e165f0a213ba79d1db34ecef9d23bcde0
wycliff byakagaba <spearkickboxing20@gmail.com>
2026-03-07 03:09:42 +0300

    week01/Exercise2/Program.cs week01/Exercise2/cse210.csproj

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
-}
+        // Ask the user to enter a number
+        Console.Write("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        // Check if the number is even or odd
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"{number} is an even number.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is an odd number.");
+        }
+            
+            if (number % 3 == 0)
+                {
+                    Console.WriteLine($"{number} is also divisible by 3.");
+                }
+            }
+        }
+    

cd462d7fe82b883a8fadf3e9142d1d199e5e4ece
wycliff byakagaba <spearkickboxing20@gmail.com>
2026-03-07 04:40:06 +0300

    week01/Exercise5/Program.cs week01/Exercise5/cse210.csproj

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,16 @@
 using System;
 
-class Program
+class Exercise5
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        //ask the user to enter a number
+        Console.Write("Please enter a number: ");
+        string input = Console.ReadLine();
+        //convert the input to an integer
+        int number = int.Parse(input);
+        //display the number
+        Console.WriteLine("You entered: " + number);
     }
 }

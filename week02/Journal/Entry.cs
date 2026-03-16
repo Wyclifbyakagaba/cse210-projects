7e7d1a7a790e3d238959449d7422431ef00cc449
wyclifbyakagaba <spearkickboxing20@gmail.com>
2026-03-16 11:07:41 +0300

    Refactor Entry class properties and display method

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,12 +1,18 @@
+using System;
+
 public class Entry
 {
     public string _date;
-    public string _promptText;
-    public string _entryText;
+    public string _prompt;
+    public string _response;
+    public string _mood;
 
     public void Display()
     {
-        Console.WriteLine($"{_date} - {_promptText}");
-        Console.WriteLine($"{_entryText}\n");
+        Console.WriteLine($"Date: {_date}");
+        Console.WriteLine($"Prompt: {_prompt}");
+        Console.WriteLine($"Response: {_response}");
+        Console.WriteLine($"Mood: {_mood}");
+        Console.WriteLine();
     }
 }

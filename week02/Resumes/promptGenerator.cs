c83ac4dcf9e564562f2128693e4878694e86c215
wyclifbyakagaba <spearkickboxing20@gmail.com>
2026-03-16 11:10:14 +0300

    Refactor prompts and improve random selection logic
    Updated the prompt list with new questions and fixed variable naming.

--- week02/Resumes/promptGenerator.cs
+++ week02/Resumes/promptGenerator.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 
 public class PromptGenerator
 {
-    private List<string> _prompts = new List<string>()
+    public List<string> _prompts = new List<string>()
     {
-        "What was the best part of your day?",
-        "What did you learn today?",
-        "Who did you talk to today?",
-        "What made you smile today?",
-        "What are you grateful for today?"
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What made me smile today?",
+        "What is something new I learned today?"
     };
 
     public string GetRandomPrompt()
     {
-        Random rand = new Random();
-        int index = rand.Next(_prompts.Count);
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+
         return _prompts[index];
     }
 }

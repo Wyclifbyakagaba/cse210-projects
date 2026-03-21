efcb36e5293e07a409565fec64d2f5aa773ad9c5
wycliff byakagaba <spearkickboxing20@gmail.com>
2026-03-22 01:53:46 +0300

    Merge branch 'main' of https://github.com/Wyclifbyakagaba/cse210-hw

--- week02/Resumes/JournalApp/journalApp/Program.cs
+++ week02/Resumes/JournalApp/journalApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace JournalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to your Journal!");
+            
+            // Example of journal input
+            Console.WriteLine("Enter your journal entry:");
+            string entry = Console.ReadLine();
+            Console.WriteLine($"You wrote: {entry}");
+        }
+    }
+}
